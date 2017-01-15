a799b238ad8ac2dcb446be8884273b7a2d691da1
Your Name <emiliano.billi@gmail.com>
2017-01-15 19:43:27 +0000

    backend

--- api/backend.py
+++ api/backend.py
@@ -45,6 +45,103 @@ class VideoAuth(object):
         else:
             raise VideoAuthException('Flens')
 
+class Components(object):
+    def __init__(self, config):
+        if 'channels' in config:
+            self.channels   = dynamodbCollection(config['channels'])
+        if 'categories' in config:
+            self.categories = dynamodbCollection(config['categories'])
+        if 'blocks' in config:
+            self.blocks     = dynamodbCollection(config['blocks'])
+        if 'sliders' in config:
+            self.sliders    = dynamodbCollection(config['sliders'])
+
+    def __query(self, where, q):
+        try:
+            ret    = where.query(q)
+            status = 200
+        except CollectionException as e:
+            status = 422
+            ret    = {'status': 'failure', 'message': str(e)}
+        except DynamoException as e:
+            ret    = {'status': 'failure', 'message': str(e)}
+            status = 500
+        except Exception as e:
+            status = 500
+            ret    = {'status': 'failure', 'message': str(e)}
+
+        return {'status': status, 'body': ret}
+
+    def __add(self, where, item):
+        try:
+            ret    = where.add(item)
+            status = 201
+        except CollectionException as e:
+            status = 422
+            ret    = {'status': 'failure', 'message': str(e)}
+        except DynamoException as e:
+            ret    = {'status': 'failure', 'message': str(e)}
+            status = 500
+        except Exception as e:
+            status = 500
+            ret    = {'status': 'failure', 'message': str(e)}
+
+        return {'status': status, 'body': ret}
+
+
+    def __del(self, where, item):
+        try:
+            ret    = where.delete(item)
+            print ret
+            status = 204
+        except CollectionException as e:
+            status = 422
+            ret    = {'status': 'failure', 'message': str(e)}
+        except DynamoException as e:
+            ret    = {'status': 'failure', 'message': str(e)}
+            status = 500
+        except Exception as e:
+            status = 500
+            ret    = {'status': 'failure', 'message': str(e)}
+
+        return {'status': status, 'body': ret}
+
+    '''
+        Add Methods for Slider, Block and Category
+    '''
+    def add_slider(self, Item={}):
+        return self.__add(self.sliders, Item)
+
+    def add_block(self, Item={}):
+        return self.__add(self.blocks, Item)
+
+    def add_category(self, Item={}):
+        return self.__add(self.categories, Item)
+
+    '''
+        Del Methods for Slider, Block and Category
+    '''
+    def del_block(self, Item={}):
+        return self.__del(self.blocks, Item)
+
+    def del_category(self, Item={}):
+        return self.__del(self.categories, Item)
+
+    def del_slider(self, Item={}):
+        return self.__del(self.sliders, Item)
+
+    '''
+        Query Methods for Slider, Block and Category
+    '''
+    def query_block(self, arg):
+        return self.__query(self.blocks,arg)
+
+    def query_slider(self, arg):
+        return self.__query(self.sliders,arg)
+
+    def query_categories(self, arg):
+        return self.__query(self.categories,arg)
+
 
 class Backend(object):
     def __init__(self, config):
@@ -199,6 +296,7 @@ class Backend(object):
         response['items'] = ni
         return response
 
+
     def __cloudsearch_query(self, lang, fqset, exclude, start, size, sort, img):
         '''
             Hace el query definitivo a Cloud Search cuando se 
@@ -301,7 +399,6 @@ class Backend(object):
                 sort  = args['sort']
             else:
                 sort  = None
-
 
             if 'img' in args:
                 img = args['img']
