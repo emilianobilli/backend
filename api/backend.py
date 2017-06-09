603cb7ccab69a46e0f034d2161aa5e84ace11770
Your Name <emiliano.billi@gmail.com>
2017-06-09 17:57:09 +0000

    target

--- api/backend.py
+++ api/backend.py
@@ -905,10 +905,16 @@ class Backend(object):
                         if ret['item'] != {}:
                             show = ret['item']
                             if 'img' in args:
-                                qret  = self.query_show({'lang':lang,'channel': show['channel'], 'img': args['img'], 'size':1000})
+                                if 'target_country' in args:
+                                    qret  = self.query_show({'lang':lang,'channel': show['channel'],'target_country':args['target_country'] ,'img': args['img'], 'size':1000})
+                                else:
+                                    qret  = self.query_show({'lang':lang,'channel': show['channel'], 'img': args['img'], 'size':1000})
                             else:
-                                qret  = self.query_show({'lang':lang,'channel': show['channel'], 'size': 1000})
-                    
+                                if 'target_country' in args:
+                                    qret  = self.query_show({'lang':lang,'channel': show['channel'],'target_country':args['target_country'] ,'size': 1000})
+                                else:
+                                    qret  = self.query_show({'lang':lang,'channel': show['channel'], 'size': 1000})
+
                     status = 200
                     ret = self.__commit_suggest(qret)
                 else:
