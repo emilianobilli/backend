53420903c4a0a990cfb741107f4770dfe9175a39
egalvan <ezgatrabajo@gmail.com>
2018-01-19 17:04:47 -0300

    Listados

--- dam/cawas/static/js/videotag.js
+++ dam/cawas/static/js/videotag.js
@@ -1,15 +1,96 @@
  jQuery(document).ready(function($)
         {
+
+            //variables
+
             var indexrow = 0;
-            $(".form-control.select2").select2({placeholder: "Despliega la lista"});
-            $("#agregartag").click();
+            var button;
+            var dataid;
+            var asset;
+            var dataout;
+            var datain;
+            var json_data;
+            var asset_id;
+            var timein;
+            var timeout;
+            var tag_id;
+            var url;
+            var timeinteger_in;
+            var timeinteger_out;
+            var videolog_id;
+            var p;
+
+        $(".form-control.select2").select2({placeholder: "Despliega la lista"});
+
+         function changeVideo(src, divId){// para cambiar lo que reproduce el player de acuerdo al ID de la lista.
+               return jwplayer(divId).setup({
+                    "file": "http://cdnlevel3.zolechamedia.net/" + src + "/mp4/1200/" + src +".mp4",
+                });
+        }
+        p = changeVideo($("#asset_id").val(),"repro1");
 
 
 
 
-            //Funcion Agregar Row
+
+
+
+        var toSecString = function(val)
+        {
+            strseg = "";
+            strmin = "";
+            strformat="";
+            min = 0;
+            seg = 0;
+            if ( val > 60 ) {
+                min = Math.round(val / 60);
+                seg = Math.round(val % 60);
+                console.log(min);
+                console.log(seg);
+            }else{
+                min = 0;
+                seg = val;
+            }
+            strmin = min.toString();
+            strseg = seg.toString();
+
+            if (min < 10 ){
+                strmin = "0" + min.toString();
+            }
+            if (seg < 10){
+                strseg = "0" + seg.toString();
+            }
+            strformat = strmin.toString() + ":" + strseg.toString();
+            return strformat;
+
+        }
+
+        formatTimeList();
+
+        function formatTimeList(){
+
+            $("td[name='inputin']").each(function() {
+                var tdin = $(this);
+                timeinteger_in = tdin.attr("data");
+                tdin.html(toSecString(timeinteger_in));
+            });
+            $("td[name='inputout']").each(function() {
+                var tdout =$(this);
+                timeinteger_out = tdout.attr("data");
+                tdout.html(toSecString(timeinteger_out));
+            });
+
+
+        }
+
+
+
+            //AGREGAR TAG
             $("#agregartag").click(function(){
                 indexrow = indexrow +1;
+
+
+
                     //Cargar con ajax el contenido de tags y generar el row
                     var tags =listaTags;
                     if(tags.length > 0)
@@ -17,176 +98,193 @@
                         var tdselect = '<td class="tb-cell"><select data-id="'+indexrow+'" class="form-control select2" size="6">';
                         for(d in tags)
                         {
-                            tdselect +='<option data="'+tags[d]['descripcion']+'">'+tags[d]['descripcion']+'</option>';
+                            tdselect +='<option data="'+tags[d]['id']+'">'+tags[d]['descripcion']+'</option>';
                         }
                         tdselect +='</select></td>';
                     }
-                    var rowtag='<tr class="tb-row" data-id="'+indexrow+'">';
-                    rowtag+= '<td class="tb-cell" >AssetID</td>';
+                    var rowtag='<tr class="tb-row" data="" data-id="'+indexrow+'">';
+
                     rowtag+= tdselect;
-                    rowtag+= '<td class="tb-cell"><button class="btn-sm btn-default"><span class="glyphicon glyphicon-play"> </span> In</button></td>';
-                    rowtag+='<td data-id="'+indexrow+'" name="inputin" class="tb-cell" data="00:01:01">00:00:00</td>';
+                    rowtag+= '<td class="tb-cell"><button class="btn-sm btn-default" data-id="'+indexrow+'" name="btnin"><span class="glyphicon glyphicon-play"> </span> In</button></td>';
+                    rowtag+='<td data-id="'+indexrow+'" name="inputin" class="tb-cell" data="">00:00</td>';
                     rowtag+='<td class="tb-cell"><button data-id="'+indexrow+'" class="btn-sm btn-default" name="btnout"><span class="glyphicon glyphicon-stop"> </span> Out</button></td>';
-                    rowtag+='<td data-id="'+indexrow+'" name="inputout" class="tb-cell" data="00:03:02" >00:00:00</td>';
-                    rowtag+='<td class="tb-cell"></td>';
+                    rowtag+='<td data-id="'+indexrow+'" name="inputout" class="tb-cell" data="">00:00</td>';
+                    rowtag+='<td class="tb-cell"><button style="display:none" data="" data-id="'+indexrow+'" name="btndelete" class="btn-md btn-danger" ><span class="glyphicon glyphicon-trash"></span>Del  </button></td>';
                     rowtag+= '</tr>';
 
                     $("#tabletags tr:last").after(rowtag);
                     $(".form-control.select2").select2({placeholder: "Despliega la lista"});
 
-                    //Click en el Boton OUT
+                    //CLICK IN
+                    $("button[name='btnin']").click(function(){
+                            var buttonin = $(this);
+                            var dataid= buttonin.attr("data-id");
+
+                            var tdin = $("td[name='inputin'][data-id='"+dataid+"']");
+
+                            var timeinteger_in = Math.round(p.getPosition());
+                            if (timeinteger_in ==0){
+                                alert("El Video no se esta reproduciendo, Esssstupido!");
+                                return false;
+                            }
+                            tdin.attr("data", timeinteger_in.toString());
+                            tdin.html(toSecString(timeinteger_in));
+
+                    });
+
+                    //CLICK OUT
                     $("button[name='btnout']").click(function(){
-
                         var button = $(this);
-                        //obtener el DATA-ID seleccionado
-                        var dataid= button.attr("data-id");
-
-                        //leer asset id
-                        strasset = "select[data-id='"+dataid+"'] option:selected";
-                        var asset = $(strasset).attr("data");
-
-                        //leer time in
-                        strin = "td[data-id='"+dataid+"'][name='inputin']";
-                        var datain = $(strin).attr("data");
-
-                        //leer time out
-                        strout = "td[data-id='"+dataid+"'][name='inputout']";
-                        var dataout = $(strout).attr("data");
-
-                        if (asset==null){
-                            alert("Debe seleccionar un asset");
-
-                        }else{
-                            alert("value"+ asset);
-                        }
-
-                        //Crear Json
-
-
+                        var index = button.attr("data-id");
                         //Enviar Json por ajax para guardar el videotag
 
-
-
-                        alert("value"+ datain);
-                        alert("value"+ dataout);
-
-
-
-                    });
-
-
-                    /*
-                    $.ajax({
-                            method: "GET",
-                            url: "/ajax/tags/",
-                            dataType: 'json',
-                            contentType: "application/text; charset=utf-8",
-                            success: function(data)
-                            {
-                                var tags =data;
-                                if(tags.length > 0)
-                                {
-                                    var tdselect = '<td class="tb-cell"><select class="form-control select2" size="6">';
-                                    for(d in tags)
-                                    {
-
-                                        tdselect +='<option data-id="'+d['id']+'" value="'+d['descripcion']+'">'+d['descripcion']+'</option>';
-                                    }
-                                    alert(d["id"]);
-                                    tdselect +='</select></td>';
-                                }
-                                var rowtag='<tr class="tb-row" data-id="'+indexrow+'">';
-                                rowtag+= '<td class="tb-cell">AssetID</td>';
-                                rowtag+= tdselect;
-                                rowtag+= '<td class="tb-cell"><button class="btn-sm btn-default"><span class="glyphicon glyphicon-play"> </span> In</button></td>';
-                                rowtag+='<td class="tb-cell">00:00:00</td>';
-                                rowtag+='<td class="tb-cell"><button class="btn-sm btn-default"><span class="glyphicon glyphicon-stop"> </span> Out</button></td>';
-                                rowtag+='<td class="tb-cell">00:00:00</td>';
-                                rowtag+='<td class="tb-cell"></td>';
-                                rowtag+= '</tr>';
-
-                                $("#tabletags tr:last").after(rowtag);
-                                $(".form-control.select2").select2({placeholder: "Despliega la lista"});
-
-
-
-                            },
-                            error: function(jqXHR, exception)
-                            {
-                                if(jqXHR.status === 405)
-                                {
-                                    console.error("METHOD NOT ALLOWED!");
-                                }
+                        var timeinteger_out = Math.round(p.getPosition());
+                        if (timeinteger_out ==0){
+                                alert("El Video no se esta reproduciendo, Esssstupido!");
+                                return false;
                             }
+                        var tdout = $("td[name='inputout'][data-id='"+index+"']");
+                        tdout.attr("data",timeinteger_out.toString());
+                        tdout.html(toSecString(timeinteger_out));
+
+                        if(validar(index)){
+                            guardar(index);
+
+                        };
                     });
-                    */
 
 
 
 
+
+                     //CLICK BOTON DELETE
+                    $("button[name='btndelete']").click(function(){
+                        var data = $(this).attr("data");
+                        deleteVideoLog(data);
+                    });
+
+
+                function validar(index){
+                    //leer asset id
+                    strasset = "select[data-id='"+index+"'] option:selected";
+                    tag_id = $(strasset).attr("data");
+                    if (tag_id==null){
+                        alert("El Tag es obligatorio.");
+                        $("select[data-id='"+index+"']").focus();
+                        return false;
+                    }
+
+                    //leer time in
+                    strin = "td[data-id='"+index+"'][name='inputin']";
+                    timein = $(strin).attr("data");
+                    if (timein==""){
+                        alert("Se debe marcar el tiempo de inicio del Tag (In).");
+                        $("button[name='btnin'][data-id='"+index+"']").focus();
+                        return false;
+
+                    }
+                    //leer time out
+                    strout = "td[data-id='"+index+"'][name='inputout']";
+                    timeout = $(strout).attr("data");
+                    if (timeout==""){
+                        alert("Se debe marcar el tiempo de inicio del Tag (Out).");
+                        $(timeout).focus();
+                        return false;
+                    }
+                    return true;
+
+                }
+
+
+
+
+    });
+
+    //Fin de Click en Agrear Tag
+
+        //AGREGAR BOTON DELETE
+        function addButtonDelete(index, id ){
+            var btndelete = $("button[name='btndelete'][data-id='"+index+"']");
+            //id es el valor de la PK dentro de la tabla VideoLog
+            btndelete.attr("data",id);
+            btndelete.show();
+
+            // se actualiza el campo de la fila para poder hacer el HIDE
+            var tr = $("tr[data-id='"+index+"']");
+            tr.attr("data",id);
+        }
+
+
+        function guardar(index){
+
+                asset_id = $("#asset_id").val();
+
+                url = "/videologs/add/"+ asset_id +"/"
+                json_data= {
+                        "tag_id": tag_id,
+                        "timein": timein,
+                        "timeout": timeout,
+                        "asset_id": asset_id
+                };
+
+                $.ajax({
+                    url: url,
+                    dataType: 'json',
+                    type: 'POST',
+                    contentType: "application/json; charset=utf-8",
+                    data: JSON.stringify( json_data ),
+                    success: function(response){
+                            videolog_id = response.data;
+                            //index es el numero de fila
+                            //videolog_id es la PK de la tabla videolog
+                            addButtonDelete(index, videolog_id)
+                            alert("Guardado Correctamente");
+
+
+
+                        },
+                    error:function(request, status, error){
+                        alert(request.responseText);
+
+                }
+                });
+            }
+
+            //ELIMINAR VIDEOLOG POR ID, FUNCION AJAX
+            function deleteVideoLog(videolog_id){
+                url = "/videologs/delete/";
+                json_data= {
+                        "id": videolog_id
+                };
+
+                $.ajax({
+                    url: url,
+                    dataType: 'json',
+                    type: 'POST',
+                    contentType: "application/json; charset=utf-8",
+                    data: JSON.stringify( json_data ),
+                    success: function(response){
+                            //alert("Eliminado Correctamente");
+                            $("tr[data='"+response.data+"']").fadeOut(1000);
+                        },
+                    error:function(request, status, error){
+                        alert(request.responseText);
+
+                    }
+                });
+
+            }
+
+
+
+
+
+            //CLICK BOTON DELETE
+            $("button[name='btndelete']").click(function(){
+                var data = $(this).attr("data");
+                deleteVideoLog(data);
             });
 
 
 
-
-
-
-
-            /*Detectar evento click */
-            $(".btn.btn-xs.btn-default.actualizaritempedido").click(function(){
-                    indexrow = indexrow +1;
-
-
-                    //Armar json
-                    var id = $(this).attr("data-id");
-                    var select = "input[data-id=" + id + "][name='cantidad']";
-                    var cantidad = $(select).val();
-                    var json_item = {
-                                    "id": id,
-                                    "cantidad": cantidad
-                                    };
-
-                        $.ajax({
-                            method: "POST",
-                            url: "",
-                            dataType: 'json',
-                            data: JSON.stringify( json_item ),
-                            success: function(data)
-                            {
-                                if(data.hasOwnProperty("response") && data.response === "success")
-                                {
-                                    if(data.hasOwnProperty("item"))
-                                    {
-                                            var posts = data.item;
-                                            if(posts.length > 0)
-                                            {
-                                                var html = "";
-                                                for(d in posts)
-                                                {
-                                                    html += "<p>" + JSON.stringify(posts[d]) + "</p>";
-                                                }
-                                                $("#containerPosts").append(html);
-                                            }
-
-                                    }
-                                    else
-                                    {
-                                        console.log("POSTS NOT FOUND");
-                                    }
-                                }
-                            },
-                            error: function(jqXHR, exception)
-                            {
-                                if(jqXHR.status === 405)
-                                {
-                                    console.error("METHOD NOT ALLOWED!");
-                                }
-                            }
-                    });
-
-
-
-            });
-
-
-        });
+    });
